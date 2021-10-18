ee3dc6a732942ce97f1f7ee2684159befeed2903
Ed Bueler <elbueler@alaska.edu>
2021-10-17 20:22:13 -0800

    trajectory.py now shows trajectories of 2 balls in x,y plane

--- makefile
+++ makefile
@@ -9,27 +9,29 @@ twoballs: twoballs.o
 	${RM} twoballs.o
 
 # use this target to create symbolic links to the scripts for
-# PETSc binary files and for plotTS.py from Bueler's book
-P4PDES_DIR = ~/repos/p4pdes/
+# PETSc binary files
 pyScripts:
 	ln -sf ${PETSC_DIR}/lib/petsc/bin/PetscBinaryIO.py
 	ln -sf ${PETSC_DIR}/lib/petsc/bin/petsc_conf.py
-	ln -sf ${P4PDES_DIR}/c/ch5/plotTS.py
 
 # testing
 runtwoballs_1:
-	-@./testit.sh twoballs "-ts_monitor binary:t.dat -ts_monitor_solution binary:u.dat -ts_adapt_dt_max 0.01" 1 1
-# view .dat files by:
-#    $ ./plotTS.py -o figure.png t.dat u.dat -dof 8 -c 1
-#    $ eog figure.png
+	-@./testit.sh twoballs "-ts_monitor -ts_view" 1 1
+
+DATFILES:
+	@./twoballs -ts_max_time 10.0 -ts_monitor binary:t.dat -ts_monitor_solution binary:u.dat -ts_adapt_dt_max 0.01 > /dev/null
+
+runtrajectory_1: DATFILES
+	-@./testit.sh trajectory.py "-o figure.png t.dat u.dat" 1 1
 
 test_twoballs: runtwoballs_1
-test: test_twoballs
+test_trajectory: runtrajectory_1
+test: test_twoballs test_trajectory
 
-.PHONY: distclean
+.PHONY: distclean DATFILES
 
 distclean:
 	@rm -f *~ twoballs *tmp
 	@rm -f *.pyc *.dat *.dat.info *.png
-	@rm -f PetscBinaryIO.py petsc_conf.py plotTS.py
+	@rm -f PetscBinaryIO.py petsc_conf.py
 	@rm -rf __pycache__/
---
+++ trajectory.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+help =\
+'''
+Plot trajectory from running TWOBALLS.  Reads output from
+   -ts_monitor binary:TDATA -ts_monitor_solution binary:UDATA
+Requires copies or sym-links to $PETSC_DIR/lib/petsc/bin/PetscBinaryIO.py and
+$PETSC_DIR/lib/petsc/bin/petsc_conf.py.  (Do:  make pyScripts)
+Note there is a particular variable order:
+  (x1, y1, dx1dt, dy1dt, x2, y2, dx2dt, dy2dt)
+'''
+
+import PetscBinaryIO
+
+from sys import exit, stdout
+from time import sleep
+from argparse import ArgumentParser, RawTextHelpFormatter
+import numpy as np
+import matplotlib.pyplot as plt
+
+parser = ArgumentParser(description=help,
+                        formatter_class=RawTextHelpFormatter)
+parser.add_argument('tfile',metavar='TDATA',
+                    help='from -ts_monitor binary:TDATA')
+parser.add_argument('ufile',metavar='UDATA',
+                    help='from -ts_monitor_solution binary:UDATA')
+parser.add_argument('-o',metavar='FILE',dest='filename',
+                    help='image file FILE for trajectory')
+args = parser.parse_args()
+
+io = PetscBinaryIO.PetscBinaryIO()
+t = np.array(io.readBinaryFile(args.tfile)).flatten()
+U = np.array(io.readBinaryFile(args.ufile)).transpose()
+dims = np.shape(U)
+
+if len(t) != dims[1]:
+    print('time dimension mismatch: %d != %d' % (len(t),dims[1]))
+    exit(1)
+
+print('time t has length=%d, solution Y is shape=(%d,%d)' % \
+          (len(t),dims[0],dims[1]))
+
+#for k in range(dims[0]):
+#    plt.plot(t,U[k],label='y[%d]' % k)
+
+plt.plot(U[0,U[1]>=0],U[1,U[1]>=0],label='particle 1')
+plt.plot(U[4,U[5]>=0],U[5,U[5]>=0],label='particle 2')
+plt.xlabel('x'),  plt.ylabel('y')
+plt.legend()
+
+if args.filename:
+    print('writing file %s' % args.filename)
+    plt.savefig(args.filename)
+else:
+    plt.show()
